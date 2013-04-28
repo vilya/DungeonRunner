19634f123196e36eea32d285ef0482e745f55ae0
Vilya Harvey <vilya.harvey@gmail.com>
2013-04-28 14:25:25 +0100

    Add a HUD

--- js/main.js
+++ js/main.js
@@ -18,17 +18,19 @@ var dungeon = function () { // start of the dungeon namespace
 
   var game = {
     // three.js objects
-    'world': null,    // The top-level Scene object for the world.
-    'dungeon': null,  // The 3D object for the dungeon.
-    'player': null,   // The 3D object for the player.
-    'camera': null,   // The 3D object for the camera.
-    'controls': null, // The current camera controls, if any.
-    'loot': [],       // The list of available loot items.
+    'world': null,      // The top-level Scene object for the world.
+    'dungeon': null,    // The 3D object for the dungeon.
+    'player': null,     // The 3D object for the player.
+    'camera': null,     // The 3D object for the camera.
+    'controls': null,   // The current camera controls, if any.
+    'loot': [],         // The list of available loot items.
+
+    'hud':  null,       // The HUD, displays player's score, life remaining, etc.
 
     'debug': false,
 
     // Information about the dungeon.
-    'dungeonCfg': {
+    'dungeonData': {
       'rows': 10,
       'cols': 10,
       'tiles': [
@@ -48,7 +50,7 @@ var dungeon = function () { // start of the dungeon namespace
     },
 
     // Information about the tiles used to build the dungeon.
-    'tileCfg': {
+    'tileData': {
       'floorGeo': new THREE.CubeGeometry(TILE_SIZE, 1, TILE_SIZE),
       'xWallGeo': new THREE.CubeGeometry(TILE_SIZE, WALL_HEIGHT, WALL_THICKNESS),
       'zWallGeo': new THREE.CubeGeometry(WALL_THICKNESS, WALL_HEIGHT, TILE_SIZE),
@@ -56,20 +58,22 @@ var dungeon = function () { // start of the dungeon namespace
     },
 
     // Information about the player.
-    'playerCfg': {
+    'playerData': {
       'autorun':  false,  // Whether the player automatically runs.
       'runSpeed': 20.0,   // The movement speed of the player, in metres/second.
       'jogSpeed': 10.0,    // The movement speed of the player, in metres/second.
       'walkSpeed': 5.0,   // The movement speed of the player, in metres/second.
       'turnSpeed': 5.0,   // How quickly the player turns, in radians/second(?).
+      'score': 0,
+      'life': 100,
     },
 
     // Information about the camera.
-    'cameraCfg': {
+    'cameraData': {
       'offset': new THREE.Vector3(0, (WALL_HEIGHT - 1) * 0.8, 10), // Position of the camera relative to the player.
     },
 
-    'lootCfg': {
+    'lootData': {
       'frequency': 0.05,
       'geo': new THREE.CylinderGeometry(0.25, 0.25, 0.1, 20, 1, false),
       'material': null, // gets initialised in makeDungeon.
@@ -84,28 +88,28 @@ var dungeon = function () { // start of the dungeon namespace
   function makeWorld()
   {
     game.world = new THREE.Scene();
-    game.world.fog = new THREE.Fog(0x000000, 10, 100);
+    game.world.fog = new THREE.Fog(0x000000, 10, 60);
   }
 
 
   function makeDungeon()
   {
-    game.dungeonCfg.width = game.dungeonCfg.cols * TILE_SIZE; // metres
-    game.dungeonCfg.depth = game.dungeonCfg.cols * TILE_SIZE; // metres
+    game.dungeonData.width = game.dungeonData.cols * TILE_SIZE; // metres
+    game.dungeonData.depth = game.dungeonData.cols * TILE_SIZE; // metres
 
-    game.tileCfg.material = new THREE.MeshLambertMaterial({
+    game.tileData.material = new THREE.MeshLambertMaterial({
       'color': 0xAAAAAA,
       'map': THREE.ImageUtils.loadTexture('img/rock.png')
     });
-    game.tileCfg.material.map.wrapS = THREE.RepeatWrapping;
-    game.tileCfg.material.map.wrapT = THREE.RepeatWrapping;
-    game.tileCfg.material.map.repeat.set(2, 2);
+    game.tileData.material.map.wrapS = THREE.RepeatWrapping;
+    game.tileData.material.map.wrapT = THREE.RepeatWrapping;
+    game.tileData.material.map.repeat.set(2, 2);
 
-    game.lootCfg.material = new THREE.MeshLambertMaterial({
+    game.lootData.material = new THREE.MeshLambertMaterial({
         'color': 0xEEEE00,
         'emissive': 0x1E1E00,
     });
-    game.lootCfg.geo.computeBoundingBox();
+    game.lootData.geo.computeBoundingBox();
 
     game.dungeon = new THREE.Object3D();
     game.world.add(game.dungeon);
@@ -114,55 +118,55 @@ var dungeon = function () { // start of the dungeon namespace
     game.dungeon.add(light);
 
     // Make the floor tiles
-    for (var r = 0, endR = game.dungeonCfg.rows; r < endR; r++) {
-      for (var c = 0, endC = game.dungeonCfg.cols; c < endC; c++) {
-        if (game.dungeonCfg.tiles[r][c] != 0)
+    for (var r = 0, endR = game.dungeonData.rows; r < endR; r++) {
+      for (var c = 0, endC = game.dungeonData.cols; c < endC; c++) {
+        if (game.dungeonData.tiles[r][c] != 0)
           _makeTile(r, c);
       }
     }
 
     // Make the +z walls
-    for (var r = 0, endR = game.dungeonCfg.rows; r < endR; r++) {
+    for (var r = 0, endR = game.dungeonData.rows; r < endR; r++) {
       var wasInTile = false;
-      for (var c = 0, endC = game.dungeonCfg.cols; c < endC; c++) {
-        var inTile = (game.dungeonCfg.tiles[r][c] != 0);
+      for (var c = 0, endC = game.dungeonData.cols; c < endC; c++) {
+        var inTile = (game.dungeonData.tiles[r][c] != 0);
         if (inTile != wasInTile)
           _makeZWall(r, c);
         wasInTile = inTile;
       }
       if (wasInTile)
-        _makeZWall(r, game.dungeonCfg.cols);
+        _makeZWall(r, game.dungeonData.cols);
     }
 
     // Make the +x walls
-    for (var c = 0, endC = game.dungeonCfg.cols; c < endC; c++) {
+    for (var c = 0, endC = game.dungeonData.cols; c < endC; c++) {
       var wasInTile = false;
-      for (var r = 0, endR = game.dungeonCfg.rows; r < endR; r++) {
-        var inTile = (game.dungeonCfg.tiles[r][c] != 0);
+      for (var r = 0, endR = game.dungeonData.rows; r < endR; r++) {
+        var inTile = (game.dungeonData.tiles[r][c] != 0);
         if (inTile != wasInTile)
           _makeXWall(r, c);
         wasInTile = inTile;
       }
       if (wasInTile)
-        _makeXWall(game.dungeonCfg.rows, c);
+        _makeXWall(game.dungeonData.rows, c);
     }
 
     // Count the number of valid tiles.
     var numTiles = 0;
-    for (var r = 0, endR = game.dungeonCfg.rows; r < endR; r++) {
-      for (var c = 0, endC = game.dungeonCfg.cols; c < endC; c++) {
-        if (game.dungeonCfg.tiles[r][c] != 0)
+    for (var r = 0, endR = game.dungeonData.rows; r < endR; r++) {
+      for (var c = 0, endC = game.dungeonData.cols; c < endC; c++) {
+        if (game.dungeonData.tiles[r][c] != 0)
           ++numTiles;
       }
     }
 
     // Distribute the loot evenly among the valid tiles.
-    var numTreasures = Math.ceil(game.dungeonCfg.rows * game.dungeonCfg.cols * game.lootCfg.frequency);
+    var numTreasures = Math.ceil(game.dungeonData.rows * game.dungeonData.cols * game.lootData.frequency);
     var tileNum = 0;
     var n = Math.floor(numTiles / numTreasures);
-    for (var r = 0, endR = game.dungeonCfg.rows; r < endR; r++) {
-      for (var c = 0, endC = game.dungeonCfg.cols; c < endC; c++) {
-        if (game.dungeonCfg.tiles[r][c] == 0)
+    for (var r = 0, endR = game.dungeonData.rows; r < endR; r++) {
+      for (var c = 0, endC = game.dungeonData.cols; c < endC; c++) {
+        if (game.dungeonData.tiles[r][c] == 0)
           continue;
         if (tileNum % n == 0 && numTreasures > 0) {
           _makeLoot(r, c);
@@ -179,14 +183,14 @@ var dungeon = function () { // start of the dungeon namespace
     var tile = new THREE.Object3D();
 
     // Create the floor
-    var floor = new THREE.Mesh(game.tileCfg.floorGeo, game.tileCfg.material);
+    var floor = new THREE.Mesh(game.tileData.floorGeo, game.tileData.material);
     floor.translateOnAxis(new THREE.Vector3(0, -0.5, 0), 1);
     floor.receiveShadow = true;
     tile.add(floor);
 
     // Create the roof
     if (!game.debug) {
-      var roof = new THREE.Mesh(game.tileCfg.floorGeo, game.tileCfg.material);
+      var roof = new THREE.Mesh(game.tileData.floorGeo, game.tileData.material);
       roof.translateOnAxis(new THREE.Vector3(0, WALL_HEIGHT + 0.5, 0), 1);
       roof.receiveShadow = true;
       tile.add(roof);
@@ -204,7 +208,7 @@ var dungeon = function () { // start of the dungeon namespace
   function _makeXWall(row, col)
   {
     // Create the wall
-    var wall = new THREE.Mesh(game.tileCfg.xWallGeo, game.tileCfg.material);
+    var wall = new THREE.Mesh(game.tileData.xWallGeo, game.tileData.material);
     wall.translateOnAxis(new THREE.Vector3(0, WALL_HEIGHT / 2.0, 0), 1);
     //wall.castShadow = true;
     wall.receiveShadow = true;
@@ -221,7 +225,7 @@ var dungeon = function () { // start of the dungeon namespace
   function _makeZWall(row, col)
   {
     // Create the wall
-    var wall = new THREE.Mesh(game.tileCfg.zWallGeo, game.tileCfg.material);
+    var wall = new THREE.Mesh(game.tileData.zWallGeo, game.tileData.material);
     wall.translateOnAxis(new THREE.Vector3(0, WALL_HEIGHT / 2.0, 0), 1);
     //wall.castShadow = true;
     wall.receiveShadow = true;
@@ -237,7 +241,7 @@ var dungeon = function () { // start of the dungeon namespace
 
   function _makeLoot(row, col)
   {
-    var loot = new THREE.Mesh(game.lootCfg.geo, game.lootCfg.material);
+    var loot = new THREE.Mesh(game.lootData.geo, game.lootData.material);
     loot.translateOnAxis(new THREE.Vector3(0, 0, 1.25), 1);
     loot.castShadow = true;
     loot.receiveShadow = true;
@@ -259,8 +263,8 @@ var dungeon = function () { // start of the dungeon namespace
 
     geo.computeBoundingBox();
 
-    var startRow = game.dungeonCfg.startTile / game.dungeonCfg.cols;
-    var startCol = game.dungeonCfg.startTile % game.dungeonCfg.cols;
+    var startRow = game.dungeonData.startTile / game.dungeonData.cols;
+    var startCol = game.dungeonData.startTile % game.dungeonData.cols;
     var startPos = tileCenter(startRow, startCol);
     startPos.y += 1;
 
@@ -284,7 +288,7 @@ var dungeon = function () { // start of the dungeon namespace
 
     game.camera = new THREE.PerspectiveCamera(fieldOfView, aspectRatio, nearClip, farClip);
     game.camera.position.copy(game.player.position);
-    game.camera.translateOnAxis(game.cameraCfg.offset, 1.0);
+    game.camera.translateOnAxis(game.cameraData.offset, 1.0);
     game.camera.lookAt(game.player.position);
     game.world.add(game.camera);
 
@@ -306,7 +310,7 @@ var dungeon = function () { // start of the dungeon namespace
   function makeDebugGrid()
   {
     var grid = new THREE.Mesh(
-        new THREE.PlaneGeometry(game.dungeonCfg.width, game.dungeonCfg.depth, game.dungeonCfg.cols, game.dungeonCfg.rows),
+        new THREE.PlaneGeometry(game.dungeonData.width, game.dungeonData.depth, game.dungeonData.cols, game.dungeonData.rows),
         new THREE.MeshBasicMaterial({
           'color': 0x8888CC,
           'wireframe': true,
@@ -314,7 +318,7 @@ var dungeon = function () { // start of the dungeon namespace
         })
     );
     grid.applyMatrix(new THREE.Matrix4().makeRotationX(Math.PI / 2));
-    grid.translateOnAxis(new THREE.Vector3(game.dungeonCfg.width / 2.0, game.dungeonCfg.depth / 2.0, 0.0), 1);
+    grid.translateOnAxis(new THREE.Vector3(game.dungeonData.width / 2.0, game.dungeonData.depth / 2.0, 0.0), 1);
     //grid.lookAt(new THREE.Vector3(0, 1, 0));
     game.world.add(grid);
 
@@ -362,12 +366,12 @@ var dungeon = function () { // start of the dungeon namespace
     var toRow = Math.floor(endPos.z / TILE_SIZE);
 
     // If you're moving off the map, you'll hit a wall.
-    if (toCol < 0 || toCol >= game.dungeonCfg.cols || toRow < 0 || toRow >= game.dungeonCfg.rows)
+    if (toCol < 0 || toCol >= game.dungeonData.cols || toRow < 0 || toRow >= game.dungeonData.rows)
       return true;
 
     // Otherwise, if you're trying to move to an empty tile you'll hit a wall.
     if (fromCol != toCol || fromRow != toRow)
-      return game.dungeonCfg.tiles[toRow][toCol] == 0;
+      return game.dungeonData.tiles[toRow][toCol] == 0;
 
     // If neither of those apply, you're good.
     return false;
@@ -428,6 +432,7 @@ var dungeon = function () { // start of the dungeon namespace
       if (overlapping(game.player, game.loot[i])) {
         game.dungeon.remove(game.loot[i]);
         game.loot.splice(i, 1);
+        game.playerData.score += 1;
       }
     }
 
@@ -441,24 +446,24 @@ var dungeon = function () { // start of the dungeon namespace
     if (ludum.isKeyPressed(ludum.keycodes.RIGHT))
       turn.y -= 1.0;
 
-    if (game.playerCfg.autorun) {
+    if (game.playerData.autorun) {
       if (ludum.isKeyPressed(ludum.keycodes.UP))
-        speed = game.playerCfg.runSpeed;
+        speed = game.playerData.runSpeed;
       else if (ludum.isKeyPressed(ludum.keycodes.DOWN))
-        speed = game.playerCfg.walkSpeed;
+        speed = game.playerData.walkSpeed;
       else
-        speed = game.playerCfg.jogSpeed;
+        speed = game.playerData.jogSpeed;
     }
     else {
       if (ludum.isKeyPressed(ludum.keycodes.UP))
-        speed = game.playerCfg.runSpeed;
+        speed = game.playerData.runSpeed;
       else if (ludum.isKeyPressed(ludum.keycodes.DOWN))
-        speed = -game.playerCfg.walkSpeed;
+        speed = -game.playerData.walkSpeed;
       else
         speed = 0;
     }
 
-    var turnAmount = game.playerCfg.turnSpeed * dt / 1000.0;
+    var turnAmount = game.playerData.turnSpeed * dt / 1000.0;
     var moveAmount = speed * dt / 1000.0;
 
     if (turn.y != 0.0)
@@ -473,9 +478,101 @@ var dungeon = function () { // start of the dungeon namespace
     else {
       game.camera.position.copy(game.player.position);
       game.camera.rotation.copy(game.player.rotation);
-      game.camera.translateOnAxis(game.cameraCfg.offset, 1.0);
+      game.camera.translateOnAxis(game.cameraData.offset, 1.0);
       game.camera.lookAt(game.player.position);
     }
+
+    refreshHUD();
+  }
+
+
+  // HUD class.
+  function HUD()
+  {
+    var x = 0;
+    var y = 0;
+    var w = 100;
+
+    this.domElement = document.createElement("div");
+    this.domElement.id = "hud";
+    this.domElement.style.width = w + "px";
+    this.domElement.style.position = 'absolute';
+    this.domElement.style.bottom = y + "px";
+    this.domElement.style.left = x + "px";
+    this.domElement.style.zIndex = 100;
+    this.domElement.style.background = "#222";
+    this.domElement.style.fontFamily = "Helvetica,Arial,sans-serif";
+    this.domElement.style.fontSize = "12px";
+    //this.domElement.style.fontWeight = "bold";
+    this.domElement.style.opacity = 0.8;
+
+    var scoreElem = document.createElement("div");
+    scoreElem.id = "score";
+    scoreElem.style.cssText = "padding:0 0 3px 3px; text-align: left;";
+    this.domElement.appendChild(scoreElem);
+
+    var scoreText = document.createElement("div");
+    scoreText.id = 'scoreText';
+    scoreText.innerHTML = "Gold";
+    scoreText.style.color = "yellow";
+    scoreElem.appendChild(scoreText);
+
+    var lifeElem = document.createElement("div");
+    lifeElem.id = 'life';
+    lifeElem.style.cssText = "padding:0 0 3px 3px; text-align: left;";
+    this.domElement.appendChild(lifeElem);
+
+    var lifeText = document.createElement("div");
+    lifeText.id = 'lifeText';
+    lifeText.innerHTML = "Life";
+    lifeText.style.color = "red";
+    lifeElem.appendChild(lifeText);
+
+    var stopwatchElem = document.createElement("div");
+    stopwatchElem.id = 'stopwatch';
+    stopwatchElem.style.cssText = "padding:0 0 3px 3px; text-align: left;";
+    this.domElement.appendChild(stopwatchElem);
+
+    var stopwatchText = document.createElement("div");
+    stopwatchText.id = 'stopwatchText';
+    stopwatchText.innerHTML = "taken";
+    stopwatchText.style.color = "#BBB";
+    stopwatchElem.appendChild(stopwatchText);
+
+    this.setScore = function (score) {
+      scoreText.textContent = score + " gold";
+    }
+
+    this.setLife = function (life) {
+      lifeText.textContent = life + "% health";
+    }
+
+    this.setStopwatch = function (timeInSecs) {
+      //var timeStr = new Date(ludum.globals.stateT).toTimeString().replace(/.*\d{2}:(\d{2}:\d{2}).*/, "$1");
+      var mins = Math.floor(timeInSecs / 60);
+      var secs = Math.floor(timeInSecs % 60);
+      var secsStr = (secs < 10) ? "0" + secs : "" + secs;
+      var str = mins + ":" + secsStr + " secs";
+      stopwatchText.textContent = str;
+    }
+
+    return this;
+  }
+
+
+  function makeHUD()
+  {
+    game.hud = new HUD();
+    document.getElementById('viewport').appendChild(game.hud.domElement);
+  }
+
+
+
+  function refreshHUD()
+  {
+    game.hud.setScore(game.playerData.score);
+    game.hud.setLife(game.playerData.life);
+    game.hud.setStopwatch(ludum.globals.stateT);
   }
 
 
@@ -518,13 +615,14 @@ var dungeon = function () { // start of the dungeon namespace
 		document.getElementById( 'viewport' ).appendChild( gRenderStats.domElement );
 		
     // Configure ludum.js
-    ludum.useKeyboard(); // Install's ludum.js' keyboard event handlers.
+    ludum.useKeyboard(); // Installs ludum.js' keyboard event handlers.
 
     // Set up the game states.
     ludum.addState('playing', { 'draw': playingDraw, 'update': playingUpdate });
 
     // Create the world, camera, etc.
     makeWorld();
+    makeHUD();
     makeDungeon();
     makePlayer();
     makeCamera();
@@ -544,9 +642,17 @@ var dungeon = function () { // start of the dungeon namespace
 
   function resize()
   {
-    game.camera.aspect = window.innerWidth / window.innerHeight;
+    var width = window.innerWidth;
+    var height = window.innerHeight;
+    var textSize = height * 0.05;
+    var x = width - (4 * textSize) - 10.0;
+
+    game.camera.aspect = width / height;
     game.camera.updateProjectionMatrix();
-    gRenderer.setSize(window.innerWidth, window.innerHeight);
+
+    refreshHUD();
+
+    gRenderer.setSize(width, height);
   }
 
 
